e16ffb558e456326c8352b7fe51b93151556dc36
Kleverson Eller <kleverson.a16@gmail.com>
2023-04-01 12:15:38 -0300

    feat: :sparkles: Criando rota (getAllGroups)
    Criando rota para a requisição de todos os grupos

--- packages/server/groups/src/routers/group.routes.ts
+++ packages/server/groups/src/routers/group.routes.ts
@@ -4,6 +4,7 @@ import prisma from "../model/prisma.connect";
 import GroupModel from "../model/group.model";
 import GroupService from "../services/group.service";
 import GroupController from "../controller/group.controller";
+import Token from '../utils/token';
 
 const model = new GroupModel(prisma);
 const service = new GroupService(model);
@@ -12,5 +13,8 @@ const controller = new GroupController(service);
 const groupRouter = Router();
 
 groupRouter.post("/create", async (req, res) => controller.newGroup(req, res));
+groupRouter.get("/getAllGroups", (req, res, next) => {
+        Token.decodedToken(req, res, next);
+}, async (req, res) => controller.getAllGroups(req, res));
 
 export default groupRouter;
