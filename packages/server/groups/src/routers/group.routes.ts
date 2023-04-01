238995fe178b8712277c0e7140621e0fa4c8f430
Kleverson Eller <kleverson.a16@gmail.com>
2023-04-01 14:04:46 -0300

    fix: :bug: Corrigindo a implementação da validação do token feita na rota errada

--- packages/server/groups/src/routers/group.routes.ts
+++ packages/server/groups/src/routers/group.routes.ts
@@ -12,9 +12,9 @@ const controller = new GroupController(service);
 
 const groupRouter = Router();
 
-groupRouter.post("/create", async (req, res) => controller.newGroup(req, res));
-groupRouter.get("/getAllGroups", (req, res, next) => {
-        Token.decodedToken(req, res, next);
-}, async (req, res) => controller.getAllGroups(req, res));
+groupRouter.post("/create", (req, res, next) => {
+    Token.decodedToken(req, res, next);
+}, async (req, res) => controller.newGroup(req, res));
+groupRouter.get("/getAllGroups", async (req, res) => controller.getAllGroups(req, res));
 
 export default groupRouter;
